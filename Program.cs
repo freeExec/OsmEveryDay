dd814879a9e5030a1c0fd3f0477c9ea6b3c5d822
freeExec <freeExec@mail.ru>
2017-01-05 16:09:58 +0400

    v0.2 - добавлено чтение напрямую из gz архивов

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 
@@ -65,7 +66,20 @@ namespace OsmEveryDay
                 return;
             }
 
-            var rowsImport = ReadChangeDayOsc(change_day);
+            var rowsImport = default(List<RowImportModel>);
+            switch(Path.GetExtension(change_day))
+            {
+                case ".osc":
+                    rowsImport = ReadChangeDayOsc(change_day);
+                    break;
+                case ".gz":
+                    rowsImport = ReadChangeDayOscGz(change_day);
+                    break;
+                default:
+                    Console.WriteLine("Not supported format detected by extention of file");
+                    return;
+            }
+            
             var rowsExport = PrepareToExport(rowsImport);
             var csv = Path.ChangeExtension(Path.GetFullPath(change_day), "csv");
             ExportToCsv(csv, rowsExport);
@@ -73,80 +87,114 @@ namespace OsmEveryDay
             Console.WriteLine("All changesets: {0}, Export: {1}", rowsImport.Count, rowsExport.Count);
         }
 
-        static List<RowImportModel> ReadChangeDayOsc(string change_day)
+        static List<RowImportModel> ReadChangeDayOscGz(string changeDayGz)
         {
             var rowsImport = new List<RowImportModel>(DEFAULT_ROWS);
-            var existsChangeset = new HashSet<long>();
-
             try
             {
-                var readerOsc = new StreamReader(change_day);
-                int count = 0;
-                long fileSize = readerOsc.BaseStream.Length;
-                long readSize = 0;
-                const int progressSize = 28;
-                long blockSize = fileSize / progressSize;
-                int newLineSize = Environment.NewLine.Length;
+                var readerOscGzRaw = new FileStream(changeDayGz, FileMode.Open);
+                int dwSize = sizeof(int);
+                readerOscGzRaw.Position = readerOscGzRaw.Length - dwSize;    // last DWORD is size len
+                var rawSize = new byte[dwSize];
+                readerOscGzRaw.Read(rawSize, 0, dwSize);
+                long length = BitConverter.ToUInt32(rawSize, 0);
+                readerOscGzRaw.Position = 0;
 
-                long nextBlock = blockSize;
+                var readerOscGz = new GZipStream(readerOscGzRaw, CompressionMode.Decompress);
+                var readerOsc = new StreamReader(readerOscGz, Encoding.UTF8);
 
-                Console.WriteLine("Read progress:");
-                for (int i = 0; i < progressSize; i++)
-                {
-                    Console.Write('░');
-                }
-                Console.Write('\r');
+                rowsImport = ParseOsc(readerOsc, length);
+                readerOsc.Close();
 
-                var splitPattern = '"';
+            }
+            catch (IOException ioex)
+            {
+                Console.WriteLine("IO READ.GZ ERROR: {0}", ioex.Message);
+            }
 
-                while (!readerOsc.EndOfStream)
-                {
-                    var line = readerOsc.ReadLine();
-                    readSize += line.Length + newLineSize;
-                    if (readSize >= nextBlock)
-                    {
-                        nextBlock += blockSize;
-                        Console.Write('▓');
-                    }
+            return rowsImport;
+        }
 
-                    if (line.Contains("<node id=") || line.Contains("<way id=") || line.Contains("<relation id="))
-                    {
-                        var splits = line.Split(splitPattern);
-                        if (splits[4] == " timestamp=" && splits[6] == " uid=" && splits[8] == " user=" && splits[10] == " changeset=")
-                        {
-                            var changeset = long.Parse(splits[11]);
-                            if (!existsChangeset.Contains(changeset))
-                            {
-                                existsChangeset.Add(changeset);
-                                rowsImport.Add(new RowImportModel
-                                {
-                                    Timestamp = splits[5],
-                                    Uid = int.Parse(splits[7]),
-                                    User = splits[9],
-                                    Changeset = changeset
-                                });
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine();
-                            Console.WriteLine("WARNING: bad line - {0}", line);
-                        }
-                    }
-
-                    count++;
-                    //if (count > 100) break;
-                }
-                Console.WriteLine();
+        static List<RowImportModel> ReadChangeDayOsc(string changeDay)
+        {
+            var rowsImport = default(List<RowImportModel>);
+            try
+            {
+                var readerOsc = new StreamReader(changeDay, Encoding.UTF8);
+                rowsImport = ParseOsc(readerOsc, readerOsc.BaseStream.Length);
                 readerOsc.Close();
             }
             catch (IOException ioex)
             {
                 Console.WriteLine("IO READ ERROR: {0}", ioex.Message);
             }
+            return rowsImport;
+        }
+
+        static List<RowImportModel> ParseOsc(StreamReader readerOsc, long fileSize)
+        {
+            var rowsImport = new List<RowImportModel>(DEFAULT_ROWS);
+            var existsChangeset = new HashSet<long>();
+
+            int count = 0;
+            //long fileSize = readerOsc.BaseStream.Length;
+            long readSize = 0;
+            const int progressSize = 28;
+            long blockSize = fileSize / progressSize;
+            int newLineSize = Environment.NewLine.Length;
+
+            long nextBlock = blockSize;
+
+            Console.WriteLine("Read progress:");
+            for (int i = 0; i < progressSize; i++)
+            {
+                Console.Write('░');
+            }
+            Console.Write('\r');
+
+            var splitPattern = '"';
+
+            while (!readerOsc.EndOfStream)
+            {
+                var line = readerOsc.ReadLine();
+                readSize += line.Length + newLineSize;
+                if (readSize >= nextBlock)
+                {
+                    nextBlock += blockSize;
+                    Console.Write('▓');
+                }
+
+                if (line.Contains("<node id=") || line.Contains("<way id=") || line.Contains("<relation id="))
+                {
+                    var splits = line.Split(splitPattern);
+                    if (splits[4] == " timestamp=" && splits[6] == " uid=" && splits[8] == " user=" && splits[10] == " changeset=")
+                    {
+                        var changeset = long.Parse(splits[11]);
+                        if (!existsChangeset.Contains(changeset))
+                        {
+                            existsChangeset.Add(changeset);
+                            rowsImport.Add(new RowImportModel
+                            {
+                                Timestamp = splits[5],
+                                Uid = int.Parse(splits[7]),
+                                User = splits[9],
+                                Changeset = changeset
+                            });
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("WARNING: bad line - {0}", line);
+                    }
+                }
+
+                count++;
+                //if (count > 100) break;
+            }
+            Console.WriteLine();
 
             existsChangeset.Clear();
-
             return rowsImport;
         }
 
@@ -187,7 +235,7 @@ namespace OsmEveryDay
         {
             try
             {
-                var writeCsv = new StreamWriter(csv);
+                var writeCsv = new StreamWriter(csv, false, Encoding.UTF8);
                 writeCsv.WriteLine("uid;user;timestamp;changesets_count");
                 foreach (var rowE in rowExport)
                 {
